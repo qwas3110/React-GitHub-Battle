8af36221c4ab8f5a67c625d20764dde82574dc05
xiehongwei <283994917@qq.com>
2019-10-10 23:59:19 +0800

    选手结果 大致显示

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa';
 import PropTypes from 'prop-types';
+import Results from "./Results";
+
 
 
 function Instructions () {
@@ -120,6 +122,7 @@ export default class Battle extends React.Component {
     this.state = {
       playerOne: null,
       playerTwo: null,
+      battle: false
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -138,7 +141,11 @@ export default class Battle extends React.Component {
 
 
   render() {
-    const { playerOne, playerTwo } = this.state
+    const { playerOne, playerTwo, battle } = this.state
+
+    if (battle === true) {
+      return <Results playerOne={playerOne} playerTwo={playerTwo} />
+    }
     return (
       <React.Fragment>
         <Instructions />
@@ -169,6 +176,14 @@ export default class Battle extends React.Component {
               />
             }
           </div>
+          {playerOne && playerTwo && (
+            <button
+              className='btn dark-btn btn-space'
+              onClick={() => this.setState({battle: true})}
+            >
+              Battle
+            </button>
+          )}
         </div>
       </React.Fragment>
     )
