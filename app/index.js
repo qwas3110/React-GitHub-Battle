31e1a71e3585d9ad393ed70072cc6e158e53b5fd
xhw <283994917@qq.com>
2019-10-30 17:51:25 +0800

    修改index them 使用hooks

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
@@ -16,39 +16,38 @@ const Battle = React.lazy(() => import('./components/Battle'));
 const Results = React.lazy(() => import('./components/Results'));
 
 
-class App extends React.Component {
-    state = {
-      theme: 'light',
-      toggleTheme: () => {
-        this.setState(({ theme }) => ({
-          theme: theme === 'light' ? 'dark' : 'light'
-        }))
-      }
-    };
+function App () {
 
-    render() {
-        return (
-          <Router>
-            <ThemeProvider value={this.state}>
-              <div className={this.state.theme}>
-                <div className='container'>
-                  <Nav />
+    const [ theme, setTheme ] = useState('light');
+    const toggleTheme = () => setTheme(
+        (theme) => theme === 'light' ? 'dark' : 'light'
+    );
 
-                  <React.Suspense fallback={<Loading />} >
-                    <Switch>
-                      <Route exact path='/' component={Popular} />
-                      <Route exact path='/battle' component={Battle} />
-                      <Route path='/battle/results' component={Results} />
-                      <Route render={() => <h1>404</h1>} />
-                    </Switch>
-                  </React.Suspense>
+    return (
+        <Router>
+            <ThemeProvider value={theme}>
+                <div className={theme}>
+                    <div className='container'>
+                        <Nav toggleTheme={toggleTheme} />
+
+                        <React.Suspense fallback={<Loading />} >
+                            <Switch>
+                                <Route exact path='/' component={Popular} />
+                                <Route exact path='/battle' component={Battle} />
+                                <Route path='/battle/results' component={Results} />
+                                <Route render={() => <h1>404</h1>} />
+                            </Switch>
+                        </React.Suspense>
+                    </div>
                 </div>
-              </div>
             </ThemeProvider>
-          </Router>
-        )
-    };
+        </Router>
+    )
 }
+
+
+
+
 
 ReactDOM.render(
     <App />,
